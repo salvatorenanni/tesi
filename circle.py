fbba557a98574c73554176b5c99892fce382d5a0
sli <salvatore.nanni001@gmail.com>
2020-10-19 09:25:21 +0200

    Necoomit 25/09

--- circle.py
+++ circle.py
@@ -8,34 +8,69 @@ from glob import glob
 import numpy as np
 import sys
 
-#
-# if len(sys.argv) < 1:
-#     print('usage: DBquery.py inputPath/ outputPath/')
-#     exit(1)
 
-# outPath = sys.argv[2]
-# inPath = sys.argv[1]
+def filters(df, elem):
+    tmp = df.loc[df['light'] > 0]
+    app_data = []
+    cx = -1
+    cy = -1
+    for i in range(len(tmp)):
+        cx = tmp.values[i][38]
+        cy = tmp.values[i][39]
+
+        data = df[(df.X - cx) ** 2 + (df.Y - cy) ** 2 < r ** 2]
+        app_data.append(data)
+
+    if cx != -1 and cy != -1:
+        app_data = pd.concat(app_data).drop_duplicates(keep='first')
+        app_data.to_csv(outPath + elem, index=False)
+
+
+if len(sys.argv) < 2:
+    print('usage: circle.py inputPath/ outputPath/')
+    exit(1)
+
+outPath = sys.argv[2]
+inPath = sys.argv[1]
 r = 20
-item = '/home/sli/Downloads/csv/wrf5_d03_20200801Z0300.nc.nc4.csv'
+
+for item in os.listdir(inPath):
+    if item.endswith(".csv"):
+        df1 = pd.read_csv(inPath + item)
+        print(item)
+        filters(df1, item)
+
+# item = '/home/sli/Downloads/csv/wrf5_d03_20200801Z1500.nc.nc4.csv'
+
+# df.drop_duplicates()
+
+
+# def filters(df):
+#     tmp = df.loc[df['light'] > 0]
+#     # appended_data = pd
+#
+#     for i in range(len(tmp)):
+#         cx = -1
+#         cy = -1
+#         cx = tmp.values[i][38]
+#         cy = tmp.values[i][39]
+#
+#         if i == 0:
+#             appended_data = df[(df.X - cx) ** 2 + (df.Y - cy) ** 2 < r ** 2]
+#         else:
+#             pd.concat([appended_data, df[(df.X - cx) ** 2 + (df.Y - cy) ** 2 < r ** 2]], ignore_index=True)
+#
+#     # appended_data = pd.concat(appended_data)
+#         if cx != -1 and cy != -1:
+#             # appended_data.drop_duplicates()
+#             appended_data.to_csv(outPath + item)
+#
+#
 # for item in os.listdir(inPath):
 #     if item.endswith(".csv"):
-df = pd.read_csv(item)
-tmp = df.loc[df['light'] == 1]
-# x = df[['X']].to_numpy()
-# y = df[['Y']].to_numpy()
-# cx = tmp['X'].to_numpy()
-# cy = tmp['Y'].to_numpy()
-
-
-for i in range(len(tmp)):
-    cx = tmp.values[i][38]
-    cy = tmp.values[i][39]
-    if i == 0:
-        row = df[(df.X - cx) ** 2 + (df.Y - cy) ** 2 < r ** 2]
-    row = row.append(df[(df.X - cx) ** 2 + (df.Y - cy) ** 2 < r ** 2], ignore_index=True)
-
-print(row)
-row.to_csv('/home/sli/Downloads/circle.csv')
+#         df1 = pd.read_csv(inPath + item)
+#         print(item)
+#         filters(df1)
 
 # Nump = df[['X', 'Y']].to_numpy()
 # coords = tmp[['X', 'Y']].to_numpy()
